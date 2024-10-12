135e0fc64f1f51419f01018160a654caa815e9d1
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 23:15:59 +0100

    updating appointment view model

--- Models/AppointmentViewModel.cs
+++ Models/AppointmentViewModel.cs
@@ -5,19 +5,16 @@
         public int? Id { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
+        public required string StartDate { get; set; }
+        public string? EndDate { get; set; }
         public int Duration { get; set; }
-        public string DoctorId { get; set; }
-        public string Patient { get; set; }
-
-        public string IsDoctorApproved { get; set; }
-        public string AdminId { get; set; }
-
-        public string DoctorName { get; set; }
-        public string PatientName { get; set; }
-        public string AdminName { get; set; }
-
-        public bool IsForClient {  get; set; }
+        public required string DoctorId { get; set; }
+        public required string Patient { get; set; }
+        public  string? IsDoctorApproved { get; set; }
+        public  string? AdminId { get; set; }
+        public  string? DoctorName { get; set; }
+        public  string? PatientName { get; set; }
+        public  string? AdminName { get; set; }
+        public bool? IsForClient {  get; set; }
     }
 }
