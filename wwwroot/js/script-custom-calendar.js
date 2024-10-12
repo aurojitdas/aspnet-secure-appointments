290f3374d34b0f8304b58d54302477c5342c5ed6
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 12:32:42 +0100

    Loading initial Calendar view

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(document).ready(function () {
+
+    InitializeCalendar();
+
+});
+function InitializeCalendar() {
+    try {
+        $('#calendar').fullCalendar({
+            timezone: true,
+            header: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            selectable: true,
+            editable: false,
+            initialView: 'dayGridMonth'
+        });
+
+    } catch (e) {
+        alert(e);
+    }
+}
