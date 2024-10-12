c9e1762a612bc0a730dc32afa7561a7923cb1ef6
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 20:10:48 +0100

    Adding status messages for api calls

--- Helper/Utility.cs
+++ Helper/Utility.cs
@@ -4,6 +4,19 @@ namespace DoctorAppointmentSchedulingApp.Helper
 {
     public static class Utility
     {
+        public static string appointmentAdded = "Appointment Added Successfully";
+        public static string appointmentUpdated = "Appointment Updated Successfully";
+        public static string appointmentDeleted = "Appointment Deleted Successfully";
+        public static string appointmentExists = "Appointment For selected Date and Time Already Exists";
+        public static string appointmentNotExists = "Appointment not exists";
+
+        public static string appointmentAddError = "Something went wrong, Please try again.";
+        public static string appointmentUpdateError = "Something went wrong, Please try again.";
+        public static string somethingWentWrong = "Something went wrong, Please try again.";
+
+        public static int success_code = 1;
+        public static int failure_code = 0;
+
         public static List<SelectListItem> GetTimeDropDown()
         {
             int minute = 60;
