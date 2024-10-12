68c8bdfd17c6a04753a97fe20b9ab3a6c66cd209
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 18:51:37 +0100

    Updating fullCalender versioin to 5.5.1

--- wwwroot/js/script-custom-calendar.js
+++ wwwroot/js/script-custom-calendar.js
@@ -4,23 +4,26 @@
 
 });
 function InitializeCalendar() {
-    try {
-        $('#calendar').fullCalendar({
-            timezone: true,
-            header: {
-                left: 'prev,next,today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            selectable: true,
-            editable: false,
-            select: function (event) {  //Creating onclick event
-                onShowModal(event, null);   
-            }
-        });
-
+    try {      
+        var calendarEl = document.getElementById('calendar');
+        if (calendarEl != null) { 
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {  //Creating onclick event
+                    onShowModal(event, null);
+                }
+            })
+            calendar.render();
+        }
     } catch (e) {
-        alert(e);
+        alert(e.stack);
     }
 }
 //Showing Modal on click
