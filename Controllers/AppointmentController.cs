8604c1339e6b10cd5f09cc704158d618f66aa482
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 09:57:28 +0100

    Adding Appointment Controller

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,19 @@
+﻿using DoctorAppointmentSchedulingApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorAppointmentSchedulingApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(AppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
