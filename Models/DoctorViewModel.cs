f5cfa361ce46f0e0e772a3a238100cdb27215c50
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 09:57:57 +0100

    Adding ViewModels for Patients and Doctor

---
+++ Models/DoctorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DoctorAppointmentSchedulingApp.Models
+{
+    public class DoctorViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
