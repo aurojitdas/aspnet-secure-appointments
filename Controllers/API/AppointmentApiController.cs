ec69bde4817da9b049f2fd1085fe468f01c1a8d2
Aurojit Das <aurojitdas@gmail.com>
2024-10-12 23:17:11 +0100

    Updating Appointment controller and Models

--- Controllers/API/AppointmentApiController.cs
+++ Controllers/API/AppointmentApiController.cs
@@ -1,4 +1,6 @@
-﻿using DoctorAppointmentSchedulingApp.Services;
+﻿using DoctorAppointmentSchedulingApp.Helper;
+using DoctorAppointmentSchedulingApp.Models;
+using DoctorAppointmentSchedulingApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -10,21 +12,44 @@ namespace DoctorAppointmentSchedulingApp.Controllers.API
     public class AppointmentApiController : Controller
     {
         private readonly IAppointmentService _appointmentService;
-        private readonly IHttpContextAccessor _httpContextAccessor;      
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly string loginUserId;
         private readonly string role;
+
         public AppointmentApiController(IAppointmentService appointmentService, IHttpContextAccessor httpContextAccessor)
         {
             _appointmentService = appointmentService;
             _httpContextAccessor = httpContextAccessor;
-
-            //For getting logged-in Userid and Roles 
             loginUserId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
             role = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+       
+        
         }
-        public IActionResult Index()
+
+        [HttpPost]
+        [Route("SaveCalendarData")]
+        public IActionResult SaveCalendarData([FromBody] AppointmentViewModel data)
         {
-            return View();
+            CommonResponse<int> commonResponse = new CommonResponse<int>();
+            try
+            {
+                commonResponse.Status = _appointmentService.AddUpdate(data).Result;
+                if (commonResponse.Status == 1)
+                {
+                    commonResponse.Message = Utility.appointmentUpdated;
+                }
+                if (commonResponse.Status == 2)
+                {
+                    commonResponse.Message = Utility.appointmentAdded;
+                }
+            }
+            catch (Exception e)
+            {
+                commonResponse.Message = e.Message;
+                commonResponse.Status = Utility.failure_code;
+            }
+
+            return Ok(commonResponse);
         }
     }
 }
--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -3,16 +3,15 @@
     public class Appointment
     {
         public int Id { get; set; }
-        public required string Title { get; set; }
-        public required string Description { get; set; }
+        public  string Title { get; set; }
+        public  string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int Duration { get; set; }
-        public string DoctorId { get; set; }
-        public string Patient {  get; set; }
-
+        public  string DoctorId { get; set; }
+        public  string Patient {  get; set; }
         public bool IsDoctorApproved { get; set; }
-        public string AdminId { get; set; }
+        public  string AdminId { get; set; }
          
 
 
