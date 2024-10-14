663b6e426646c461a0598f08a2a0ca7dc0ed3a2f
Aurojit Das <aurojitdas@gmail.com>
2024-10-14 22:33:48 +0100

    Adding new Vlaues for Patient View Model

--- Models/PatientViewModel.cs
+++ Models/PatientViewModel.cs
@@ -4,5 +4,13 @@
     {
         public string Id { get; set; }
         public string Name { get; set;}
+
+        public string Address { get; set; }
+
+        public string MedicalId { get; set; }
+        
+        public string username {  get; set; }
+
+        public string phoneNumber { get; set; }
     }
 }
