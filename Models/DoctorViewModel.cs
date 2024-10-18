7c19cabe15b10433f4d48e521ca4c2d0e3b629cc
Aurojit Das <aurojitdas@gmail.com>
2024-10-18 08:49:40 +0100

    updating doctor view model

--- Models/DoctorViewModel.cs
+++ Models/DoctorViewModel.cs
@@ -4,5 +4,10 @@
     {
         public string Id { get; set; }
         public string Name { get; set; }
+
+        public string assignedTo { get; set; }
+        public bool? assignmentStatus { get; set; }
+
+        public string userName { get; set; }
     }
 }
