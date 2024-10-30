9fe96c7b120a8776457a617b7216f376fb765390
Aurojit Das <aurojitdas@gmail.com>
2024-10-30 16:21:03 +0000

    making username and id name as required

--- Models/PatientViewModel.cs
+++ Models/PatientViewModel.cs
@@ -1,16 +1,21 @@
-﻿namespace DoctorAppointmentSchedulingApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoctorAppointmentSchedulingApp.Models
 {
     public class PatientViewModel
     {
         public string Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set;}
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
-        public string MedicalId { get; set; }
-        
+        public string? MedicalId { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
         public string username {  get; set; }
 
-        public string phoneNumber { get; set; }
+        public string? phoneNumber { get; set; }
     }
 }
